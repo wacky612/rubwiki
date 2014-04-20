c418dfc01f77333865de9afbeb21650b6a2559a3
wacky <wacky@kmc.gr.jp>
2014-04-20 17:30:58 +0900

    リクエストごとに git レポジトリのインスタンスを再生成するようにした。

--- lib/app.rb
+++ lib/app.rb
@@ -19,20 +19,21 @@ module RubWiki
     config_file "#{File.dirname(__FILE__)}/../config/config.yml"
     set :views, "#{File.dirname(__FILE__)}/../views"
     set :public_folder, "#{File.dirname(__FILE__)}/../public"
-    set :wiki, Git.new(settings.git_repo_path)
 
     get '/' do
-      @list = settings.wiki.ls()
+      wiki = Git.new(settings.git_repo_path)
+      @list = wiki.ls()
       @contents = haml(erb(:list))
       return haml(:page)
     end
 
     get '/*/edit' do
+      wiki = Git.new(settings.git_repo_path)
       path = params[:splat].first
-      if settings.wiki.exist?("#{path}.md")
-        halt unless settings.wiki.file?("#{path}.md")
-        @sha = settings.wiki.sha("#{path}.md")
-        @raw_data = settings.wiki.read("#{path}.md")
+      if wiki.exist?("#{path}.md")
+        halt unless wiki.file?("#{path}.md")
+        @sha = wiki.sha("#{path}.md")
+        @raw_data = wiki.read("#{path}.md")
       else
         @sha = ""
         @raw_data = ""
@@ -42,36 +43,39 @@ module RubWiki
     end
 
     get '/*/' do
+      wiki = Git.new(settings.git_repo_path)
       dir = params[:splat].first
-      halt unless settings.wiki.dir?(dir)
-      @list = settings.wiki.ls(dir)
+      halt unless wiki.dir?(dir)
+      @list = wiki.ls(dir)
       @contents = haml(erb(:list))
       return haml(:page)
     end
 
     get '/*' do
+      wiki = Git.new(settings.git_repo_path)
       path = params[:splat].first
       if File.extname(path).empty?
-        halt unless settings.wiki.exist?("#{path}.md")
-        raw_data = settings.wiki.read("#{path}.md")
+        halt unless wiki.exist?("#{path}.md")
+        raw_data = wiki.read("#{path}.md")
         @contents = markdown(raw_data)
         return haml(:page)
       else
-        halt unless settings.wiki.exist?(path)
-        return settings.wiki.read(path)
+        halt unless wiki.exist?(path)
+        return wiki.read(path)
       end
     end
 
     post '/*/commit' do
+      wiki = Git.new(settings.git_repo_path)
       path = params[:splat].first
       new_raw_data = params[:data]
       commit_message = params[:commit_message]
       sha1 = params[:sha]
-      sha2 = settings.wiki.sha("#{path}.md")
+      sha2 = wiki.sha("#{path}.md")
 
       if sha1 == sha2
-        settings.wiki.write("#{path}.md", NKF.nkf("-Luw", new_raw_data))
-        settings.wiki.commit(remote_user(), "#{remote_user()}@kmc.gr.jp", commit_message)
+        wiki.write("#{path}.md", NKF.nkf("-Luw", new_raw_data))
+        wiki.commit(remote_user(), "#{remote_user()}@kmc.gr.jp", commit_message)
         redirect to(URI.encode("/#{path}"))
       else
         return "conflict"
