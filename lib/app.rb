d341d3dbc11b929c7b38966a83d51a1409ff5315
wacky <wacky@kmc.gr.jp>
2014-04-24 20:43:51 +0900

    view まわりの部分を分離

--- lib/app.rb
+++ lib/app.rb
@@ -22,22 +22,16 @@ module RubWiki
 
     get '/' do
       wiki = Git.new(settings.git_repo_path)
-      @list = wiki.ls()
-
-      @nav = haml(:nav)
-      @article = haml(:list)
-      return haml(:page)
+      list = wiki.ls()
+      return list(list)
     end
 
     get '/*/history' do
       wiki = Git.new(settings.git_repo_path)
       path = params[:splat].first
       path = "#{path}.md" if File.extname(path).empty?
-      @commits = wiki.history(path)
-
-      @nav = haml(:nav)
-      @article = haml(:history)
-      return haml(:page)
+      commits = wiki.history(path)
+      return history(commits)
     end
 
     get '/*/revision/*' do
@@ -46,9 +40,7 @@ module RubWiki
       revision = params[:splat].last
       raw_data = wiki.read_from_oid(revision)
       if File.extname(path).empty?
-        @nav = haml(:nav)
-        @article = markdown(raw_data)
-        return haml(:page)
+        return revision(raw_data, path, revision)
       else
         begin
           content_type MIME::Types.type_for(path)[0].to_s
@@ -64,11 +56,8 @@ module RubWiki
       path = params[:splat].first
       oid1 = params[:splat][1]
       oid2 = params[:splat][2]
-      @diff = wiki.diff(oid1, oid2)
-
-      @nav = haml(:nav)
-      @article = haml(:diff)
-      return haml(:page)
+      diff = wiki.diff(oid1, oid2)
+      return diff(diff)
     end
 
     get '/*/edit' do
@@ -76,95 +65,78 @@ module RubWiki
       path = params[:splat].first
       if wiki.exist?("#{path}.md")
         halt unless wiki.file?("#{path}.md")
-        @oid = wiki.oid("#{path}.md")
-        @raw_data = wiki.read("#{path}.md")
+        oid = wiki.oid("#{path}.md")
+        raw_data = wiki.read("#{path}.md")
       else
-        @oid = ""
-        @raw_data = ""
+        oid = ""
+        raw_data = ""
       end
-
-      @nav = haml(:nav)
-      @form = haml(:form)
-      @article = haml(:edit)
-      return haml(:page)
+      return edit(raw_data, oid)
     end
 
     get '/*/' do
       wiki = Git.new(settings.git_repo_path)
       dir = params[:splat].first
       halt unless wiki.dir?(dir)
-      @list = wiki.ls(dir)
-
-      @nav = haml(:nav)
-      @article = haml(:list)
-      return haml(:page)
+      list = wiki.ls(dir)
+      return list(list)
     end
 
     get '/*' do
       wiki = Git.new(settings.git_repo_path)
-      @path = params[:splat].first
-      if File.extname(@path).empty?
-        halt unless wiki.exist?("#{@path}.md")
-        raw_data = wiki.read("#{@path}.md")
-        @nav = haml(:nav)
-        @article = markdown(raw_data)
-        return haml(:page)
+      path = params[:splat].first
+      if File.extname(path).empty?
+        halt unless wiki.exist?("#{path}.md")
+        raw_data = wiki.read("#{path}.md")
+        return view(raw_data, path)
       else
-        halt unless wiki.exist?(@path)
-        content_type MIME::Types.type_for(@path)[0].to_s
-        return wiki.read(@path)
+        halt unless wiki.exist?(path)
+        content_type MIME::Types.type_for(path)[0].to_s
+        return wiki.read(path)
       end
     end
 
     post '/*/commit' do
       wiki = Git.new(settings.git_repo_path)
       path = params[:splat].first
-      new_raw_data = params[:data]
+      raw_data_from_web = NKF.nkf("-Luw", params[:data])
       commit_message = params[:commit_message]
       oid_from_web = params[:oid]
       oid_from_git = wiki.oid("#{path}.md")
 
       if oid_from_web == oid_from_git
-        wiki.write("#{path}.md", NKF.nkf("-Luw", new_raw_data))
+        wiki.write("#{path}.md", raw_data_from_web)
         wiki.commit(remote_user(), "#{remote_user()}@kmc.gr.jp", commit_message)
         redirect to(URI.encode("/#{path}"))
       else
-        old_raw_data = wiki.read_from_oid(oid_from_web)
+        raw_data_old = wiki.read_from_oid(oid_from_web)
         raw_data_from_git = wiki.read("#{path}.md")
-        raw_data_from_web = NKF.nkf("-Luw", new_raw_data)
+        raw_data_merged = nil
         Dir.chdir("/tmp") do
           File.write("web", raw_data_from_web)
-          File.write("old", old_raw_data)
+          File.write("old", raw_data_old)
           File.write(oid_from_git, raw_data_from_git)
           IO.popen("merge -p web old #{oid_from_git}", "r", :encoding => Encoding::UTF_8) do |io|
-            @raw_data = io.read
+            raw_data_merged = io.read
           end
           File.delete("web")
           File.delete("old")
           File.delete(oid_from_git)
         end
         if $? == 0
-          wiki.write("#{path}.md", @raw_data)
+          wiki.write("#{path}.md", raw_data_merged)
           wiki.commit(remote_user(), "#{remote_user()}@kmc.gr.jp", commit_message)
           redirect to(URI.encode("/#{path}"))
         else
-          @form = haml(:form)
-          @nav = haml(:nav)
-          @article = haml(:conflict)
-          return haml(:page)
+          return conflict(raw_data_merged, oid_from_git)
         end
       end
     end
 
     post '/*/preview' do
-      @raw_data = params[:data]
-      @oid = params[:oid]
-
-      @nav = haml(:nav)
-      @form = haml(:form)
-      @preview = markdown(@raw_data)
-      @article = haml(:preview)
-      return haml(:page)
+      raw_data = params[:data]
+      oid = params[:oid]
+      return preview(raw_data, oid)
     end
 
     private
@@ -175,6 +147,62 @@ module RubWiki
       else
         return "anonymous"
       end
+    end
+
+    def diff(diff)
+      nav = haml :nav, locals: { path: nil }
+      article = haml :diff, locals: { diff: diff }
+      return page(nav, article)
+    end
+
+    def list(list)
+      nav = haml :nav, locals: { path: nil }
+      article = haml :list, locals: { list: list }
+      return page(nav, article)
+    end
+
+    def history(commits)
+      nav = haml :nav, locals: { path: nil }
+      article = haml :history, locals: { commits: commits }
+      return page(nav, article)
+    end
+
+    def conflict(raw_data, oid)
+      nav = haml :nav, locals: { path: nil }
+      form = haml :form, locals: { raw_data: raw_data, oid: oid }
+      article = haml :conflict, locals: { form: form }
+      return page(nav, article)
+    end
+
+    def preview(raw_data, oid)
+      nav = haml :nav, locals: { path: nil }
+      form = haml :form, locals: { raw_data: raw_data, oid: oid }
+      preview = markdown(raw_data)
+      article = haml :preview, locals: { form: form, preview: preview }
+      return page(nav, article)
+    end
+
+    def edit(raw_data, oid)
+      nav = haml :nav, locals: { path: nil }
+      form = haml :form, locals: { raw_data: raw_data, oid: oid }
+      article = haml :edit, locals: { form: form }
+      return page(nav, article)
+    end
+
+    def view(raw_data, path)
+      nav = haml :nav, locals: { path: path }
+      article = markdown(raw_data)
+      return page(nav, article)
+    end
+
+    def revision(raw_data, path, oid)
+      nav = haml :nav, locals: { path: nil }
+      article = markdown(raw_data)
+      return page(nav, article)
+    end
+
+    def page(nav, article)
+      return haml :page, locals: { nav: nav, article: article }
     end
 
   end
