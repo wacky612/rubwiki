2d52c12f2627485682f240e241a8064c146fc73b
wacky <wacky@kmc.gr.jp>
2014-04-27 17:43:14 +0900

    IRC通知の機能を設定ファイルで無効にできるように

--- lib/app.rb
+++ lib/app.rb
@@ -138,7 +138,7 @@ module RubWiki
       if oid_from_web == oid_from_git
         wiki.write(append_ext(path), raw_data_from_web)
         wiki.commit(remote_user(), remote_user_mail(), commit_message)
-        irc_notify(path, remote_user(), commit_message) if is_notify
+        irc_notify(path, remote_user(), commit_message) if is_notify && settings.irc[:enable]
         redirect to(URI.encode("/#{path}"))
       else
         raw_data_old = wiki.read_from_oid(oid_from_web)
@@ -147,7 +147,7 @@ module RubWiki
         if is_success
           wiki.write(append_ext(path), raw_data_merged)
           wiki.commit(remote_user(), remote_user_mail(), commit_message)
-          irc_notify(path, remote_user(), commit_message) if is_notify
+          irc_notify(path, remote_user(), commit_message) if is_notify && settings.irc[:enable]
           redirect to(URI.encode("/#{path}"))
         else
           return conflict(raw_data_merged, path, oid_from_git)
